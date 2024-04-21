f44529f29a935cffc64936729c2a951582f3a822
Mateusz Poręba <mateusz.poreba@outlook.com>
2024-04-21 15:14:18 +0200

    w8 4 npm i

--- setup-flashcard.py
+++ setup-flashcard.py
@@ -7,12 +7,18 @@ script_dir = os.path.dirname(script_path)
 def open_vscode_and_run_ng(directory):
     # Uruchamianie Visual Studio Code i zapisywanie PID
     vscode_process = subprocess.Popen(["C:\\Users\\vagrant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe", directory])
-    # Uruchamianie terminala i zapisywanie PID
-    terminal_process = subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && npm i && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+    # Uruchamianie terminala z poleceniem npm i i czekanie na jego zakończenie
+    npm_process = subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && npm i"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    npm_process.wait()  # Czekaj na zakończenie instalacji pakietów npm
+
+    # Po zakończeniu npm i, uruchamianie Angulara
+    ng_process = subprocess.Popen(["cmd", "/K", "cd /d " + directory + " && ng s -o"], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
     # Zapisywanie PID do pliku
     with open("process_pids.txt", "w") as file:
-        file.write(f"{vscode_process.pid}\n")
-        file.write(f"{terminal_process.pid}\n")
+        file.write(f"VSCode PID: {vscode_process.pid}\n")
+        file.write(f"npm install PID: {npm_process.pid}\n")
+        file.write(f"Angular serve PID: {ng_process.pid}\n")
 
 open_vscode_and_run_ng(script_dir)
